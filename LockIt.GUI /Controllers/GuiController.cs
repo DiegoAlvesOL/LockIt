18ddfb7d0d4f827d338be0f2c72dfab4f996c2fe
Diego Alves de Oliveira <diego@Diegos-MacBook-Air-2.local>
2025-10-30 21:24:25 +0000

    feat(gui): integrate GUI with backend controller for password saving and generation
    - Added GuiController to connect Avalonia UI with core logic
    - Implemented SaveCredential() to store data via JsonStorage
    - Added _controller instance in MainWindow for backend communication
    - Created event handlers for Copy, Reload, and Save buttons
    - Temporarily added static password generation for testing
    - Ensured data binding updates dynamically through INotifyPropertyChanged

---
+++ LockIt.GUI /Controllers/GuiController.cs
@@ -0,0 +1,45 @@
+using System;
+using LockIt.LockItCore;
+using LockIt.LockItInfra;
+
+namespace LockIt.GUI.Controllers;
+
+
+/// <summary>
+/// Classe que atua como uma ponte entre a interface gráfica (GUI) e o backend.
+/// É responsável por receber comandos da interface (como "gerar senha" ou "salvar credencial")
+/// e chamar as classes do Core e Infra para realizar essas ações.
+/// </summary>
+public class GuiController
+{
+    /// <summary>
+    /// Método responsável por gerar uma nova senha.
+    /// </summary>
+    /// <param name="length"></param>
+    /// <returns></returns>
+    public string GenerateNewPassword(int length)
+    {
+        // Aqui definimos quais conjuntos de caracteres serão usados.
+        // Para simplificar nesta aula, vamos incluir todos.
+        string charset = CharacterSets.Digits +
+                         CharacterSets.LowerCaseLetters +
+                         CharacterSets.UpperCaseLetters +
+                         CharacterSets.SpecialCharacters;
+                         
+        PasswordGenerator generator = new PasswordGenerator(charset);
+        return generator.GeneratePassword(length);
+    }
+
+    public void SaveCredential(string service, string username, string password, string? notes)
+    {
+        Credential newCredential = new Credential
+        {
+            Service = service,
+            UserName = username,
+            Password = password,
+            CreatedAt = DateTime.UtcNow,
+            Notes = notes
+        };
+        JsonStorage.Save(newCredential);
+    }
+}
